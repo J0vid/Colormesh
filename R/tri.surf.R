cd48c1f13844fffde0574323432ab4a9fe9d0641
David Aponte <jovid@Davids-MacBook-Pro.local>
2021-01-18 14:59:19 -0700

    method for plotting with tri.surf

--- R/tri.surf.R
+++ R/tri.surf.R
@@ -66,6 +66,35 @@ tri.surf <- function(tri.object, point.map, num.passes, corresponding.image, fli
   return(tri.surf.object)
 }
 
+#' plotting method for objects of class "tri.surf.points"
+#'
+#' @method plot
+#' @param x an object of class "tri.surf.points"
+#' @param style the type of plot to generate. There are currently 2 options, "points" and "triangulation". "points" is the default and just plots the interior and perimeter points. "triangulation" plots the delaunay triangulation wireframe with centroids highlighted.
+#' @param ... Additional plotting parameters to be passed to plot.default
+#' @return A list of class tri.surf.points. $interior is the position of internal (non-perimeter) points generated from triangulation. $perimeter is the initial points submitted for triangulation. $centroids is the final set of centroids from the triangulation. $final.mesh is the last round of triangulation. $point.map is the point map used to give the order of perimeter landmarks.
+#' @export
+plot.tri.surf.points <- function(x, style = "points",...){
+  if(style == "points"){
+    plot(x$perimeter, ylim = rev(range(x$perimeter[,2])), asp = 1, ...)
+    points(x$interior, ...)
+  }
+
+  if(style == "triangulation"){
+    tri.object <- rbind(x$perimeter[x$point.map,], x$interior)
+    are.you.in <- point.in.polygon(x$centroids[,1], x$centroids[,2], x$perimeter[x$point.map,1], x$perimeter[x$point.map,2]) #index for out of boundary triangles caused by concavities
+    plot(x$perimeter, typ = "n", ylab = "", xlab = "", asp = 1, axes = F, ylim = rev(range(x$perimeter[,2])), ...)
+    points(x$centroids[are.you.in == 1,], col = 2, pch = 19, cex = .25, ...)
+
+    for(j in c(1:nrow(triangles(x$final.mesh)))[are.you.in==1]){
+      lines(rbind(tri.object[triangles(x$final.mesh)[j,1],], tri.object[triangles(x$final.mesh)[j,2],]))
+      lines(rbind(tri.object[triangles(x$final.mesh)[j,1],], tri.object[triangles(x$final.mesh)[j,3],]))
+      lines(rbind(tri.object[triangles(x$final.mesh)[j,2],], tri.object[triangles(x$final.mesh)[j,3],]))
+    }
+  }
+
+}
+
 
 
 
