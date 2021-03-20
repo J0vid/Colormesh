1fa13bfe9590a9a4db18ce9e0223213c987b80a2
David Aponte <jovid@Davids-MacBook-Pro.local>
2021-03-20 14:27:44 -0600

    bug fixing documentation

--- R/rgb.measure.R
+++ R/rgb.measure.R
@@ -10,8 +10,11 @@
 #' #load landmarks and covariate data
 #' guppy.lms <- tps2array(system.file("extdata", "original_lms.TPS", package = "Colormesh"))
 #' specimen.factors <- read.csv(system.file("extdata", "specimen_factors.csv", package = "Colormesh"), header = F)
+#'consensus <- tps2array(system.file("extdata", "consensus_LM_coords.TPS", package = "Colormesh"))
+#'test.image <- load.image(paste0(path.package("Colormesh"),"/extdata/unwarped_images/GPHP_unw_001.jpg"))
+#'delaunay.map <- tri.surf(consensus, point.map = c(1,8:17,2, 18:19,3,20:27,4, 28:42,5,43:52,6,53:54,7,55:62), 3, test.image)
 #'
-#'rgb.test <- rgb.measure(imagedir = paste0(path.package("Colormesh"),"/extdata/unwarped_images/"), image.names = specimen_factors[,2], delaunay.map = delaunay.map, linearize.color.space = F)
+#'rgb.test <- rgb.measure(imagedir = paste0(path.package("Colormesh"),"/extdata/unwarped_images/"), image.names = specimen.factors[,2], delaunay.map = delaunay.map, linearize.color.space = F)
 #'
 #'plot(rgb.test, individual = 5)
 #'plot(rgb.test, individual = 5, style = "comparison")
